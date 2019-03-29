28db2ad58f1f3275e95c1e66aa4e757fc853a610
李俊 <lijun@lijundeMacBook-Air.local>
2019-03-29 10:57:52 +0800

    update

---
+++ 设计模式/观察者模式/index.js
@@ -0,0 +1,41 @@
+var EventCenter = (function () {
+    var events = {};
+
+    // 绑定事件 添加回调
+    function on(evt, handler) {
+        events[evt] = events[evt] || [];
+        events[evt].push({
+            handler: handler
+        })
+    }
+
+    function fire(evt, arg) {
+        if (!events[evt]) {
+            return
+        }
+        for (var i = 0; i < events[evt].length; i++) {
+            events[evt][i].handler(arg);
+        }
+    }
+
+    function off(evt) {
+        delete events[evt];
+    }
+
+    return {
+        on: on,
+        fire: fire,
+        off: off
+    }
+}());
+
+var number = 1;
+EventCenter.on('click', function (data) {
+    console.log('click 事件' + data + number++ + '次');
+});
+EventCenter.off('click');   //  只绑定一次
+EventCenter.on('click', function (data) {
+    console.log('click 事件' + data + number++ + '次');
+});
+
+EventCenter.fire('click', '绑定');
