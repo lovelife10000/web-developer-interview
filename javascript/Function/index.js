7b6db240da70ae7094ef5ea8da45cf291b45b3fb
李俊 <lijun@lijundeMacBook-Air.local>
2019-04-01 17:05:49 +0800

    update

--- javascript/Function/index.js
+++ javascript/Function/index.js
@@ -1,10 +1,4 @@
-/*
-* 参数变量是默认声明的，所以不能用let或const再次声明
-* */
-// function foo(x = 5) {
-//     let x = 1; // error
-//     const x = 2; // error
-// }
+
 
 /*
 * 指定了默认值以后，函数的length属性，将返回没有指定默认值的参数个数。也就是说，指定了默认值后，length属性将失真。
@@ -13,3 +7,9 @@
 // (function (a = 5) {}).length // 0
 // (function (a, b, c = 5) {}).length // 2
 
+
+let a=new Function('jj');
+console.dir(a);
+
+console.dir(Function)
+//Function是最底层的一个构造函数
--- javascript/Promise/Promise.js
+++ javascript/Promise/Promise.js
@@ -1,20 +1,55 @@
 let promise = new Promise(function (resolve, reject) {
 
     resolve('666');
-    //reject('777')//如果这个函数的参数是另一个promise实例，只有等到该promise状态为resolved或者rejected才会开始执行该reject函数
+    // reject('777')//如果这个函数的参数是另一个promise实例，只有等到该promise状态为resolved或者rejected才会开始执行该reject函数
+    debugger
     console.log('这里还会执行，即使前面已经状态改为rejected或者resolved')
 });
 
-promise.then(function (result) {
+promise.finally(function () {
+    console.log('这里表示不管状态变成啥，都会执行');
+    console.log('finally方法的回调函数不接受任何参数，这意味着没有办法知道，前面的 Promise 状态到底是fulfilled还是rejected。这表明，finally方法里面的操作，应该是与状态无关的，不依赖于 Promise 的执行结果。');
+    debugger
+}).then(function (result) {
     console.log(result);
+    debugger
     return 888
 }).then(function (result) {
-    throw ('这里发生错误，catch也能捕捉到')
+    debugger
+    throw ('虽然promise状态已经变成resolved,但是这里发生错误，catch也能捕捉到')
+    debugger
     console.log(result);
+}).then(null,function (err){
+    console.log('这样then函数也能代替catch,这样一来后面的catch就不执行了')
+    y+2
+    debugger
 }).catch(function (err) {
     debugger
-    console.log(err)
-})
+    console.log(err);
+
+}).finally(function () {
+    console.log('这里表示不管状态变成啥，都会执行');
+    console.log('finally方法的回调函数不接受任何参数，这意味着没有办法知道，前面的 Promise 状态到底是fulfilled还是rejected。这表明，finally方法里面的操作，应该是与状态无关的，不依赖于 Promise 的执行结果。');
+    debugger
+});
+
+
+Promise.all();
+Promise.race();
+Promise.resolve();
+const jsPromise = Promise.resolve($.ajax('/whatever.json'));
+
+
+
+Promise.resolve('foo')
+// 等价于
+new Promise(resolve => resolve('foo'))
+
+
+
+Promise.reject()
+
+Promise.try()
 
 //resolve和reject在函数最后执行，所以一般加上return resolve()
 //建议采用链式写法，不然会报错 ，reject写在最后
