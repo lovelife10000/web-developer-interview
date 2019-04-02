529f687feb105ecdb118222c7f77d06460e32495
李俊 <lijun@lijundeMacBook-Air.local>
2019-04-02 16:41:23 +0800

    update

---
+++ 安全/index.js
@@ -0,0 +1,15 @@
+
+
+
+
+document.getElementById('ul').addEventListener('click',function(e) {
+    switch (e.target.id){
+        case 'li1':
+            alert('li1');
+        case 'li2':
+            alert('li2');
+        case 'li3':
+            alert('li3');
+    }
+
+},false);
