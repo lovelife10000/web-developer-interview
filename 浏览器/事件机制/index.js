5a7942cbd54b9b6e50e832a13b05da83f328c98b
李俊 <lijun@lijundeMacBook-Air.local>
2019-04-02 11:05:49 +0800

    update

---
+++ 浏览器/事件机制/index.js
@@ -0,0 +1,19 @@
+document.getElementById('root').addEventListener('click',function () {
+    console.log('冒泡，这里产生了特例，也就是说，实际事件发生的元素身上，如果同时绑定了捕获和冒泡事件，哪个代码写在前面，哪个就先执行，这个时候已经不按照先捕获后冒泡的规则了');
+},false)
+document.getElementById('root').addEventListener('click',function () {
+    console.log('捕获，这里产生了特例，也就是说，实际事件发生的元素身上，如果同时绑定了捕获和冒泡事件，哪个代码写在前面，哪个就先执行，这个时候已经不按照先捕获后冒泡的规则了');
+},true)
+
+window.addEventListener('click',function () {
+    console.log('window冒泡');
+},false)
+window.addEventListener('click',function () {
+    console.log('window捕获');
+},true)
+document.getElementById('rootf').addEventListener('click',function () {
+    console.log('冒泡f');
+},false)
+document.getElementById('rootf').addEventListener('click',function () {
+    console.log('捕获f');
+},true)
