abc485de225be7e5a0490e0e45f7033edfab2214
李俊 <lijun@lijundeMacBook-Air.local>
2019-04-12 18:20:24 +0800

    update

---
+++ 浏览器/事件循环/index.js
@@ -0,0 +1,20 @@
+// setTimeout(function () {
+//     console.log(1);
+// },2000);
+//
+// var promise =new Promise(function (resolve,reject) {
+//     console.log(2);
+//     resolve();
+// });
+// promise.then(function () {
+//     console.log(3)
+// }).catch(function () {
+//     console.log(4);
+// });
+//
+
+
+
+class Abc {
+
+}
